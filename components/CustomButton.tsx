0970d8c7586a1d5ab40dcd621ff3f7bc2194bea4
bong <bongchok45@gmail.com>
2026-05-11 23:42:45 +0900

    update : 커스텀 버튼 바리에이션 추가

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -4,26 +4,36 @@ import { Pressable, PressableProps, Text, View } from "react-native";
 interface CustomButtonProps extends PressableProps {
   label: string;
   backgroundColor?: string;
+  variant?: "xl" | "lg" | "md" | "sm";
   icon?: ReactNode;
-  color: string;
+  color?: string;
 }
+
+const variantStyle = {
+  xl: { height: "h-[51px]", text: "text-lg" },
+  lg: { height: "h-[41px]", text: "text-base" },
+  md: { height: "h-[38px]", text: "text-sm" },
+  sm: { height: "h-[28px]", text: "text-xs" },
+};
 
 export default function CustomButton({
   label,
   icon,
-  color,
+  variant = "xl",
+  color="#F6F6F6",
   backgroundColor,
   ...props
 }: CustomButtonProps) {
+  const { height, text } = variantStyle[variant];
   return (
     <Pressable
-      className={`rounded-lg justify-center items-center active:opacity-80 w-full h-[51px] ${color}`}
+      className={`rounded-xl justify-center items-center active:opacity-80 w-full ${height}`}
       style={backgroundColor ? { backgroundColor } : undefined}
       {...props}
     >
       <View className="flex-row items-center gap-x-2">
         {icon}
-        <Text className="text-lg font-bold" style={{ color }}>
+        <Text className={`font-bold ${text}`} style={{ color }}>
           {label}
         </Text>
       </View>
