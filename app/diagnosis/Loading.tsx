3873d23e4a98c826684c3f80c1332270491122d8
전수안 <dgsw.ahn@dgsw.hs.kr>
2026-05-12 08:19:45 +0900

    feat : result screen

--- app/diagnosis/Loading.tsx
+++ app/diagnosis/Loading.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { Text, View } from "react-native";
 import Animated, { useAnimatedStyle, useSharedValue, withRepeat, withTiming, Easing } from 'react-native-reanimated';
 import AnimatedCheck from "./AnimatedCheck";
+import Top from "@/components/common/Top";
 
 type status = 'loading' | 'done';
 
@@ -20,18 +21,21 @@ export default function Loading() {
   }))
   
   return (
-    <View className="flex-col items-center justify-center flex-1 p-10 bg-white">
-      {status === 'loading' 
-        ? <>
-            <Animated.Image source={require("@/assets/sandClock.png")} style={animatedStyle} />
-            <Text className="text-2xl font-bold mb-[10px] mt-[30px]">콜포비아 레벨을 계산 중이에요.</Text>
-            <Text className="color-[#5C5E5E] font-medium text-base">잠시만 기다려 주세요.</Text>
-          </>
-        : <>
-            <AnimatedCheck/>
-            <Text className="text-2xl font-bold mb-[10px] mt-[30px]">콜포비아 레벨의 계산이 끝났어요.</Text>
-            <Text className="color-[#5C5E5E] font-medium text-base">함께 결과를 확인해볼까요?</Text>
-          </>}
+    <View className="flex-1 px-10 bg-white">
+      <Top title="자가진단"/>
+      <View className="flex-col items-center justify-center flex-1 mb-[105px]">
+        {status === 'loading' 
+          ? <>
+              <Animated.Image source={require("@/assets/sandClock.png")} style={animatedStyle} />
+              <Text className="text-2xl font-bold mb-[10px] mt-[30px]">콜포비아 레벨을 계산 중이에요.</Text>
+              <Text className="color-[#5C5E5E] font-medium text-base">잠시만 기다려 주세요.</Text>
+            </>
+          : <>
+              <AnimatedCheck/>
+              <Text className="text-2xl font-bold mb-[10px] mt-[30px]">콜포비아 레벨의 계산이 끝났어요.</Text>
+              <Text className="color-[#5C5E5E] font-medium text-base">함께 결과를 확인해볼까요?</Text>
+            </>}
+      </View>
     </View>
   )
 }
