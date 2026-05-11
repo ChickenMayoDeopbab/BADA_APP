9c6b19efbbacca77008741bc4651e3feb05eb0c1
전수안 <dgsw.ahn@dgsw.hs.kr>
2026-05-11 19:39:29 +0900

    update : change images and route

--- constants/diagnosis.ts
+++ constants/diagnosis.ts
@@ -1,12 +1,14 @@
 import { ImageSourcePropType } from "react-native";
 
 export const DIAGNOSIS: ImageSourcePropType[] = [
-  require("@/assets/q1_s.png"),
-  require("@/assets/q2_s.png"),
-  require("@/assets/q3_s.png"),
-  require("@/assets/q4_s.png"),
-  require("@/assets/q5_s.png"),
-  require("@/assets/q6_s.png"),
-  require("@/assets/q7_s.png"),
-  require("@/assets/q8_s.png"),
+  require("@/assets/Q1_s.png"),
+  require("@/assets/Q2_s.png"),
+  require("@/assets/Q3_s.png"),
+  require("@/assets/Q4_s.png"),
+  require("@/assets/Q5_s.png"),
+  require("@/assets/Q6_s.png"),
+  require("@/assets/Q7_s.png"),
+  require("@/assets/Q8_s.png"),
+  require("@/assets/Q9_s.png"),
+  require("@/assets/Q10_s.png"),
 ];
