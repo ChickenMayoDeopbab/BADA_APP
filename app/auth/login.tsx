3ee304c46c6dd432cb771f34be86f9333abe5957
bong <bongchok45@gmail.com>
2026-05-27 23:14:40 +0900

    feat : login API 연결

--- app/auth/login.tsx
+++ app/auth/login.tsx
@@ -1,3 +1,4 @@
+import { postLogin } from "@/api/authApi";
 import BadaLogo from "@/assets/badaLogo2.svg";
 import CustomButton from "@/components/common/CustomButton";
 import CustomInput from "@/components/common/CustomInput";
@@ -5,9 +6,6 @@ import Ionicons from "@expo/vector-icons/Ionicons";
 import { router } from "expo-router";
 import { useState } from "react";
 import {
-  KeyboardAvoidingView,
-  Platform,
-  ScrollView,
   Text,
   TouchableOpacity,
   useWindowDimensions,
@@ -16,7 +14,6 @@ import {
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function LoginScreen() {
-
   const { height, width } = useWindowDimensions();
   const isTablet = width >= 600;
   const topPadding = Math.min(Math.max(height * 0.08, 56), 80);
@@ -24,112 +21,113 @@ export default function LoginScreen() {
   const [isPasswordVisible, setIsPasswordVisible] = useState<boolean>(false);
   const [isChecked, setIsChecked] = useState<boolean>(false);
 
-  const [userId, setUserId] = useState<string>("");
+  const [username, setUsername] = useState<string>("");
   const [password, setPassword] = useState<string>("");
+
+  const handleLogin = async () => {
+    try {
+      const response = await postLogin({ username, password });
+
+      if (response.status === 200) {
+        router.push("/home");
+      } else {
+        console.log(response.error?.message);
+      }
+    } catch (error: any) {
+      console.log("에러 내용:", error.response?.data);
+    }
+  };
 
   return (
     <SafeAreaView className="flex-1 bg-white">
-      <KeyboardAvoidingView
-        behavior={Platform.OS === "ios" ? "padding" : undefined}
-        className="flex-1"
+      <View
+        className="justify-between flex-1 px-8"
+        style={{
+          paddingTop: topPadding,
+          width: "100%",
+          maxWidth: isTablet ? 430 : undefined,
+          alignSelf: "center",
+          minHeight: height,
+        }}
       >
-        <ScrollView
-          keyboardShouldPersistTaps="handled"
-          showsVerticalScrollIndicator={false}
-          contentContainerStyle={{ flexGrow: 1 }}
-        >
-          <View
-            className="justify-between flex-1 px-8"
-            style={{
-              paddingTop: topPadding,
-              width: "100%",
-              maxWidth: isTablet ? 430 : undefined,
-              alignSelf: "center",
-              minHeight: height,
-            }}
-          >
-            <View className="flex-1">
-              <BadaLogo width={70} height={32} />
-              <Text className=" text-3xl font-bold text-[#0D0D0E]">
-                아이디로 로그인
-              </Text>
-            </View>
+        <View className="flex-1">
+          <BadaLogo width={70} height={32} />
+          <Text className=" text-3xl font-bold text-[#0D0D0E]">
+            아이디로 로그인
+          </Text>
+        </View>
 
-            <View className="flex-1" style={{ marginBottom: formBottomMargin }}>
-              <View className="mb-5">
-                <CustomInput
-                  value={userId}
-                  onChangeText={setUserId}
-                  label="아이디"
-                />
-                <CustomInput
-                  value={password}
-                  onChangeText={setPassword}
-                  label="비밀번호"
-                  secureTextEntry={!isPasswordVisible}
-                  rightIcon={
-                    <TouchableOpacity
-                      onPress={() => setIsPasswordVisible(!isPasswordVisible)}
-                    >
-                      <Ionicons
-                        name={isPasswordVisible ? "eye-off-sharp" : "eye"}
-                        size={20}
-                        color="#BDBEBE"
-                      />
-                    </TouchableOpacity>
-                  }
-                />
-              </View>
-
-              <TouchableOpacity
-                className="flex-row items-center mb-6 gap-x-2"
-                onPress={() => setIsChecked(!isChecked)}
-              >
-                <View>
-                  <Ionicons
-                    name={
-                      isChecked
-                        ? "checkmark-circle"
-                        : "checkmark-circle-outline"
-                    }
-                    size={24}
-                    style={{ width: 24, height: 24 }}
-                    color={isChecked ? "#0AE365" : "#BDBEBE"}
-                  />
-                </View>
-                <Text
-                  className={`text-base ${isChecked ? "text-[#0D0D0E]" : "text-[#BDBEBE]"}`}
+        <View className="flex-1" style={{ marginBottom: formBottomMargin }}>
+          <View className="mb-5">
+            <CustomInput
+              value={username}
+              onChangeText={setUsername}
+              label="아이디"
+            />
+            <CustomInput
+              value={password}
+              onChangeText={setPassword}
+              label="비밀번호"
+              secureTextEntry={!isPasswordVisible}
+              rightIcon={
+                <TouchableOpacity
+                  onPress={() => setIsPasswordVisible(!isPasswordVisible)}
                 >
-                  로그인 상태 유지
-                </Text>
-              </TouchableOpacity>
-              <View className="gap-y-3">
-                <CustomButton
-                  label="로그인"
-                  color="#F6F6F6"
-                  backgroundColor="#0AE365"
-                  onPress={() => router.replace("/home")}
-                />
-                <CustomButton
-                  label="회원가입"
-                  color="#0D0D0E"
-                  backgroundColor="#F8F8F8"
-                  onPress={() => router.replace("/auth/signup")}
-                />
-              </View>
-
-              <View className="flex-row mt-3 gap-x-4">
-                <TouchableOpacity>
-                  <Text className="text-sm text-[#5C5E5E]">아이디 찾기</Text>
+                  <Ionicons
+                    name={isPasswordVisible ? "eye-off-sharp" : "eye"}
+                    size={20}
+                    color="#BDBEBE"
+                  />
                 </TouchableOpacity>
-                <TouchableOpacity>
-                  <Text className="text-sm text-[#5C5E5E]">비밀번호 찾기</Text>
-                </TouchableOpacity>
-              </View>
-            </View>
+              }
+            />
           </View>
-        </ScrollView>
-      </KeyboardAvoidingView>
+
+          <TouchableOpacity
+            className="flex-row items-center mb-6 gap-x-2"
+            onPress={() => setIsChecked(!isChecked)}
+          >
+            <View>
+              <Ionicons
+                name={
+                  isChecked ? "checkmark-circle" : "checkmark-circle-outline"
+                }
+                size={24}
+                style={{ width: 24, height: 24 }}
+                color={isChecked ? "#0AE365" : "#BDBEBE"}
+              />
+            </View>
+            <Text
+              className={`text-base ${isChecked ? "text-[#0D0D0E]" : "text-[#BDBEBE]"}`}
+            >
+              로그인 상태 유지
+            </Text>
+          </TouchableOpacity>
+          <View className="gap-y-3">
+            <CustomButton
+              label="로그인"
+              color="#F6F6F6"
+              backgroundColor="#0AE365"
+              onPress={handleLogin}
+            />
+            <CustomButton
+              label="회원가입"
+              color="#0D0D0E"
+              backgroundColor="#F8F8F8"
+              onPress={() => router.replace("/auth/signup")}
+            />
+          </View>
+
+          <View className="flex-row mt-3 gap-x-4">
+            <TouchableOpacity>
+              <Text className="text-sm text-[#5C5E5E]">아이디 찾기</Text>
+            </TouchableOpacity>
+            <TouchableOpacity>
+              <Text className="text-sm text-[#5C5E5E]">비밀번호 찾기</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
     </SafeAreaView>
   );
 }
