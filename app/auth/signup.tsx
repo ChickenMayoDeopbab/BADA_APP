fabea691e96e249723eb17b3e0ac7efa42138714
bong <bongchok45@gmail.com>
2026-05-27 23:14:57 +0900

    feat : signup API 연결

--- app/auth/signup.tsx
+++ app/auth/signup.tsx
@@ -1,16 +1,12 @@
+import { postSignup } from "@/api/authApi";
 import BadaLogo from "@/assets/badaLogo2.svg";
-import NicknameStep from "@/components/authSteps/NicknameStep";
+import EmailStep from "@/components/authSteps/EmailStep";
 import PasswordStep from "@/components/authSteps/PasswordStep";
 import UsernameStep from "@/components/authSteps/UsernameStep";
+
+import { router } from "expo-router";
 import { useState } from "react";
-import {
-  KeyboardAvoidingView,
-  Platform,
-  ScrollView,
-  Text,
-  useWindowDimensions,
-  View,
-} from "react-native";
+import { Text, useWindowDimensions, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function SignupScreen() {
@@ -22,66 +18,67 @@ export default function SignupScreen() {
 
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
-  const [nickname, setNickname] = useState<string>("");
+  const [username, setUsername] = useState<string>("");
+
+  const handleSignup = async () => {
+    try {
+      const response = await postSignup({ email, password, username });
+
+      if (response.status === 200) {
+        router.push("/auth/login");
+      } else {
+        console.log(response.error?.message);
+      }
+    } catch (error: any) {
+      console.log("에러 내용:", error.response?.data);
+    }
+  };
 
   return (
     <SafeAreaView className="flex-1 bg-white">
-      <KeyboardAvoidingView
-        behavior={Platform.OS === "ios" ? "padding" : undefined}
-        className="flex-1"
+      <View
+        className="justify-between flex-1 px-8"
+        style={{
+          paddingTop: topPadding,
+          width: "100%",
+          maxWidth: isTablet ? 430 : undefined,
+          alignSelf: "center",
+          minHeight: height,
+        }}
       >
-        <ScrollView
-          keyboardShouldPersistTaps="handled"
-          showsVerticalScrollIndicator={false}
-          contentContainerStyle={{ flexGrow: 1 }}
-        >
-          <View
-            className="justify-between flex-1 px-8"
-            style={{
-              paddingTop: topPadding,
-              width: "100%",
-              maxWidth: isTablet ? 430 : undefined,
-              alignSelf: "center",
-              minHeight: height,
-            }}
-          >
-            <View className="flex-1">
-              <BadaLogo width={70} height={32} />
-              <Text className=" text-3xl font-bold text-[#0D0D0E]">
-                회원가입
-              </Text>
-            </View>
+        <View className="flex-1">
+          <BadaLogo width={70} height={32} />
+          <Text className=" text-3xl font-bold text-[#0D0D0E]">회원가입</Text>
+        </View>
 
-            <View className="flex-1" style={{ marginBottom: formBottomMargin }}>
-              {step === 1 && (
-                <UsernameStep
-                  email={email}
-                  setEmail={setEmail}
-                  onNext={() => setStep(2)}
-                />
-              )}
+        <View className="flex-1" style={{ marginBottom: formBottomMargin }}>
+          {step === 1 && (
+            <EmailStep
+              email={email}
+              setEmail={setEmail}
+              onNext={() => setStep(2)}
+            />
+          )}
 
-              {step === 2 && (
-                <PasswordStep
-                  password={password}
-                  setPassword={setPassword}
-                  onPrev={() => setStep(1)}
-                  onNext={() => setStep(3)}
-                />
-              )}
+          {step === 2 && (
+            <PasswordStep
+              password={password}
+              setPassword={setPassword}
+              onPrev={() => setStep(1)}
+              onNext={() => setStep(3)}
+            />
+          )}
 
-              {step === 3 && (
-                <NicknameStep
-                  nickname={nickname}
-                  setNickname={setNickname}
-                  onPrev={() => setStep(2)}
-                  onNext={() => setStep(4)}
-                />
-              )}
-            </View>
-          </View>
-        </ScrollView>
-      </KeyboardAvoidingView>
+          {step === 3 && (
+            <UsernameStep
+              username={username}
+              setUsername={setUsername}
+              onPrev={() => setStep(2)}
+              onNext={handleSignup}
+            />
+          )}
+        </View>
+      </View>
     </SafeAreaView>
   );
 }
