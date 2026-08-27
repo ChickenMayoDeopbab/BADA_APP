80bc9294a9c68fddc9d8525d599ecf6c7fa8b50e
bong <bongchok45@gmail.com>
2026-08-27 11:08:04 +0900

    fix : 커뮤니티 바텀 탭 및 하단 여백 오류 수정

--- app/(tabs)/(community)/_layout.tsx
+++ app/(tabs)/(community)/_layout.tsx
@@ -1,14 +1,9 @@
 import { Stack } from "expo-router";
-import { CommunityProvider } from "@/context/CommunityContext";
 
 export const unstable_settings = {
   initialRouteName: "community",
 };
 
 export default function CommunityLayout() {
-  return (
-    <CommunityProvider>
-      <Stack screenOptions={{ headerShown: false }} />
-    </CommunityProvider>
-  );
+  return <Stack screenOptions={{ headerShown: false }} />;
 }
